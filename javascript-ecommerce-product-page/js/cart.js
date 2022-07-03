f1304cf618bd0a0bd3eb712af70b0b719fa22a95
LindseyGunderson <65924727+LindseyGunderson@users.noreply.github.com>
2022-07-03 12:24:57 -0700

    Adding in completed Product card
    Adding product card completed project.

--- javascript-ecommerce-product-page/js/cart.js
+++ javascript-ecommerce-product-page/js/cart.js
@@ -1,23 +1,33 @@
+
+// get elements for changing the amount
 const amtBtns = document.querySelectorAll(".product__checkout button"),
         cartQty = document.querySelector('.qty');
 
-console.log(amtBtns);
+// get the elements for showing the shopping cart
+const cartBtn = document.querySelector('#cartBtn'),
+        showCart = document.querySelector('.cart-container');
+    
+function toggleShoppingCart(e) {
+
+    showCart.classList.toggle('show-cart');
+    
+
+}
 
 let amount = 0;
 
+// handle the increase and decrease when buttons are clicked
 function handleCartAmt(e) {
 
-    console.log(e.currentTarget.id);
-
-    // determine increase or decrease
+    // if the decrease button is clicked
     if(e.currentTarget.id === 'decrease-qty'){
 
+        // check if the amount is 0 and if it is, do nothing. Or -1 from the amount.
         amount === 0 ? "" : amount--;
-        
-        
-        
+       
     } else {
 
+        // other option, increase by + 1
         amount++;
         
     }
@@ -26,4 +36,12 @@ function handleCartAmt(e) {
 
 }
 
+// add event listener for increase/decrease
 amtBtns.forEach(button => button.addEventListener('click', handleCartAmt));
+
+
+cartBtn.addEventListener('click', (e) => {
+
+    e.currentTarget === e.target && toggleShoppingCart();
+
+})
